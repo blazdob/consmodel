a162ede1bfcd64d981282db87ede8d0ce16832c3
blazdob <bldobr@gmail.com>
2023-11-06 23:49:40 +0100

    changed minor things to support open-meteo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "consmodel",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "Blaž Dobravec",
     author_email = "blaz@dobravec.si",
     description = "The library aims to provide a simple way to create individual consumer loads, generation.",
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages = setuptools.find_packages(),
-    install_requires = ['setuptools>=42', 'pvlib>=0.9.1', 'pandas>=1.5.2', 'numpy>=1.22.4', 'tzfpy>=0.15.1', 'meteostat>=1.6.5', 'scipy>=1.10.0', 'hplib==1.9'],
+    install_requires = ['setuptools>=42', 'retry_requests>=2.0.0', 'openmeteo_requests>=1.1.0', 'requests_cache>=1.1.0', 'pvlib>=0.9.1', 'pandas>=1.5.2', 'numpy>=1.22.4', 'tzfpy>=0.15.1', 'meteostat>=1.6.5', 'scipy>=1.10.0', 'hplib==1.9'],
     python_requires = ">=3.6"
 )
