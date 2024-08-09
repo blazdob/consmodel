f43f8fa0b680d9dcc4c6c1d8dfc487d7fad521fb
blazdob <bldobr@gmail.com>
2024-08-09 09:28:05 +0200

    updated the limitations of the battery system parameters being zero

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="consmodel",
-    version="0.1.6",
+    version="0.1.7",
     author="Blaž Dobravec",
     author_email="blaz@dobravec.si",
     description=
