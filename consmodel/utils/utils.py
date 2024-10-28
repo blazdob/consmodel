ab71213f8f7565e560c298a197daf37e0839257f
blazdob <bldobr@gmail.com>
2024-10-28 13:52:21 +0100

    update the datetime that i missed.

--- consmodel/utils/utils.py
+++ consmodel/utils/utils.py
@@ -5,7 +5,7 @@ def extract_first_date_of_month(df):
     # Convert the datetime column to datetime type
     df['date_time'] = pd.to_datetime(df['date_time'])
     # Extract the first date of every month by grouping and resetting index
-    first_dates = list(df.loc[df.groupby((df['date_time'] - pd.Timedelta(minutes=15)).dt.to_period('M'))['date_time'].idxmin()].reset_index(drop=True)["datetime"])
+    first_dates = list(df.loc[df.groupby((df['date_time'] - pd.Timedelta(minutes=15)).dt.to_period('M'))['date_time'].idxmin()].reset_index(drop=True)["date_time"])
 
     # Display the result
     return first_dates
