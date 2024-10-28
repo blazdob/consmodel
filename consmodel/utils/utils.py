22a8725399810bd49869d227dbd6dbbdf78fc792
blazdob <bldobr@gmail.com>
2024-10-28 13:48:57 +0100

    update an error with naming datetime to date_time

--- consmodel/utils/utils.py
+++ consmodel/utils/utils.py
@@ -3,10 +3,9 @@ import pandas as pd
 def extract_first_date_of_month(df):
     """Function returns list of first date of every month from the input dataframe."""
     # Convert the datetime column to datetime type
-    df['datetime'] = pd.to_datetime(df['datetime'])
-
+    df['date_time'] = pd.to_datetime(df['date_time'])
     # Extract the first date of every month by grouping and resetting index
-    first_dates = list(df.loc[df.groupby((df['datetime'] - pd.Timedelta(minutes=15)).dt.to_period('M'))['datetime'].idxmin()].reset_index(drop=True)["datetime"])
+    first_dates = list(df.loc[df.groupby((df['date_time'] - pd.Timedelta(minutes=15)).dt.to_period('M'))['date_time'].idxmin()].reset_index(drop=True)["datetime"])
 
     # Display the result
     return first_dates
